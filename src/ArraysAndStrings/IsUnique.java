414e26602902543e8457c346cab55749c349f52f
nullmage <jsky.johnson@gmail.com>
2020-10-27 20:26:32 -0600

    Adds tests

--- src/ArraysAndStrings/IsUnique.java
+++ src/ArraysAndStrings/IsUnique.java
@@ -1,6 +1,37 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Test;
+
+/**
+ * Question 1.1 IsUnique
+ * Implement an algorithm to determine if a string has all unique characters. 
+ * What if you cannot use additional data structures?
+ */
 public class IsUnique {
 	public static void main(String[] args){
 		System.out.println("Hello");
+	}
+
+	/**
+	 * 
+	 * @param s, a string of values between A - 0...
+	 * @return boolean value for if the string is unique or not
+	 */
+	public boolean isUnique(String s){
+		int checker = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int val = s.charAt(i) - 'a';
+			if ((checker & (1 << val)) > 0) return false;
+			checker |= (1 << val);
+		}
+		return true;
+	}
+
+	@Test
+	public void testIsUnique(){
+		assertEquals(false, isUnique("hello"));
+		assertEquals(true, isUnique("abcd"));
+		assertEquals(true, isUnique("12savd45"));
+
 	}
 }
--- src/Sandbox/Sample.java
+++ src/Sandbox/Sample.java
@@ -1,13 +1,32 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Sample a simple test class
+ */
 public class Sample {
 
-	public int add(int a, int b){
+	// public static void main(String[] args) {
+	// 	System.out.println("Hello this is a Sample");
+
+	// 	Sample t = new Sample();
+	// 	System.out.println("Testing add");
+	// 	System.out.println(t.add(3, 4));
+	// }
+
+	/**
+	 * @param a
+	 * @param b
+	 * @return the addition of a and b
+	 */
+	public int add(int a, int b) {
 		return a + b;
 	}
-	public static void main(String[] args) {
-		System.out.println("Hello this is a Sample");
 
-		Sample t = new Sample();
-		System.out.println("Testing add");
-		System.out.println(t.add(3, 4));
+	//Tests
+	@Test
+	public void testAdd() {
+		assertEquals(25, add(20, 5));
 	}
 }
