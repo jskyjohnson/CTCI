1b267a13557f06469989b2a45d12ae83f2c42ee7
nullmage <jsky.johnson@gmail.com>
2021-02-06 13:46:09 -0700

    Adds comp

--- src/test/java/com/jsky/core/ArraysAndStrings/StringCompressionTest.java
+++ src/test/java/com/jsky/core/ArraysAndStrings/StringCompressionTest.java
@@ -7,18 +7,16 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Question 1.6 String Compression
- * Implement a method to perform basic string compression using the 
- * counts of repeated characters. If the compressed string would not 
+ * Implement a method to perform basic string compression using the
+ * counts of repeated characters. If the compressed string would not
  * become smaller than the original, your method should return the original
  */
 
-
 public class StringCompressionTest {
-	@DisplayName("String Compression Test")
-	@Test
-	public void testPalindrome() {
-		assertEquals("a2b1c5a3", StringCompression.compressString("aabcccccaaa"));
-  }
-	
-}
 
+  @DisplayName("String Compression Test")
+  @Test
+  public void testPalindrome() {
+    assertEquals("a2b1c5a3", StringCompression.compressString("aabcccccaaa"));
+  }
+}
